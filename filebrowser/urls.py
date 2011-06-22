44b1d8ef11829effdf6d8102b052b2c22c57a197
Evgeny V. Generalov <e.generalov@gmail.com>
2011-06-22 20:16:35 +0600

    kill the django-uploadify dependency when USE_UPLOADIFY is False

--- filebrowser/urls.py
+++ filebrowser/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from filebrowser import settings
 
 urlpatterns = patterns('',
     
@@ -6,10 +7,14 @@ urlpatterns = patterns('',
     url(r'^browse/$', 'filebrowser.views.browse', name="fb_browse"),
     url(r'^mkdir/', 'filebrowser.views.mkdir', name="fb_mkdir"),
     url(r'^upload/', 'filebrowser.views.upload', name="fb_upload"),
-    url(r'^upload_flash/', 'filebrowser.uploadify_views.upload', name="fb_upload_flash"),
     url(r'^edit/$', 'filebrowser.views.edit', name="fb_edit"),
     url(r'^rename/$', 'filebrowser.views.rename', name="fb_rename"),
     url(r'^delete/$', 'filebrowser.views.delete', name="fb_delete"),
     url(r'^versions/$', 'filebrowser.views.versions', name="fb_versions"),
     
 )
+
+if settings.USE_UPLOADIFY:
+    urlpatterns += patterns('',
+        url(r'^upload_flash/', 'filebrowser.uploadify_views.upload', name="fb_upload_flash"),
+    )
